eea0f8ff4a3dbbdb9309b63ce8b406022179990e
Sindy Tatiana Suarez Baron <sitysb@gmail.com>
2025-07-22 21:18:19 -0500

    cree css segundo punto

--- Punto2/script.js
+++ Punto2/script.js
@@ -1,9 +1,11 @@
 let totalItems = 0;
 let totalPrecio = 0;
+let arrreglo = [];
 
 function agregarCarrito(nombre, precio){
     totalItems ++;
     totalPrecio += precio;  
+    arrreglo.push(nombre);
     
     document.getElementById("total-items").textContent = totalItems;
     document.getElementById("total-precio").textContent = totalPrecio;
@@ -11,5 +13,15 @@ function agregarCarrito(nombre, precio){
     if (totalPrecio > 50){
         alert("¡Envío gratis aplicado!")
     }
-    
+    console.log("Productos en el carrito: ", arrreglo );
+}
+function vaciarCarrito(){
+    totalItems = 0;
+    totalPrecio = 0;
+    arrreglo = [];
+
+    document.getElementById("total-items").textContent = "0";
+    document.getElementById("total-precio").textContent = "0";
+
+    alert("Carrito vacío");
 }
